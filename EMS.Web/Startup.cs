89627675b541e83125050918996a529642fa7c79
Vishal Ravlani <Vishal Ravlani>
2016-07-30 18:50:32 +0530

    Initial commit

---
+++ EMS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EMS.Web.Startup))]
+namespace EMS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
